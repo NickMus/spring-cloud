ffc914cc02ee6df844f6ad9d6be7d0ed18b90f45
nmusi <n.musinov@gmail.com>
2021-10-31 14:36:14 +0300

    Initial commit

---
+++ eureka-client/src/main/java/com/flamexander/cloud/client/GreetingController.java
@@ -0,0 +1,20 @@
+package com.flamexander.cloud.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    @RequestMapping("/greeting")
+    String greeting();
+
+    @RequestMapping("/greeting1")
+    String greeting1();
+
+    @RequestMapping("/greeting-example")
+    String greetingExample();
+
+    @RequestMapping("/get-obj")
+    Cat getCat();
+
+    @RequestMapping("/")
+    String showHomePage();
+}
